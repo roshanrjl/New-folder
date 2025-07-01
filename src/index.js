53a10e7a026dc1b02ff072bd82033b66268456cc
sachinthapa572 <121659364+sachinthapa572@users.noreply.github.com>
2025-07-01 21:22:02 +0545

    feat: implement express-validator and environment variable validation
      This commit includes:
       - Addition of express-validator for robust input validation.
       - Creation of a validation middleware to handle validation errors.
       - Implementation of environment variable validation on application startup.
       - Updated README.md with setup instructions and API documentation.
       - Creation of .env.example for easier environment setup.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDb from "./db/connect.js";
+import { validateEnv } from "./utils/validateEnv.js";
 
-import dotenv from 'dotenv'
-import connectDb from './db/connect.js'
-import app from './app.js'
+dotenv.config({
+  path: "./.env",
+});
 
-dotenv.config(
-    {path:'./.env'}
-)
+validateEnv();
 
 connectDb()
-.then(()=>{
-    app.listen(process.env.PORT || 8000 ,()=>{
-        console.log(`server is running at port:${process.env.PORT}`)
-    })
-})
-.catch((error)=>{
-    console.log("connection faild:",error)
-
-
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("connection faild:", error);
+  });
