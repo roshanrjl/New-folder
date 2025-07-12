408f53cf12bbcbd31a47318e60dccd9a6e891d10
roshanrjl <roshanrijal700@gmail.com>
2025-07-12 08:13:11 +0545

    feat: Refactor route files to include JWT verification and organize controller imports

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -13,6 +13,12 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 router.use(verifyJWT)
 
+router.route("/toggle/comment/:commentId").get(toggleCommentLike)
+router.route("/toggle/Tweet/:tweetId").get(toggleTweetLike)
+router.route("/toggle/video/:videoId").get(toggleVideoLike)
+
+router.route("/").get(getLikedVideos)
+
 
 
 export default router;
